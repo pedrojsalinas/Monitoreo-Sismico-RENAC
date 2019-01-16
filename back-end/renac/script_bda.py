bf7ff359d8d4e3c27fc7c75f7748fd1aeaf2a09f
Pedro Salinas <pedrosalinas5@gmail.com>
2019-01-16 00:47:43 -0500

    se agrego parroquias a bd

--- back-end/renac/script_bda.py
+++ back-end/renac/script_bda.py
@@ -4,8 +4,14 @@
 import csv
 from api.models import *
 
-with open('datos/cantones.csv') as csvfile:
+# with open('datos/cantones.csv') as csvfile:
+#     reader = csv.DictReader(csvfile)
+#     for row in reader:
+#         canton = Canton.objects.create(nombre=row['canton'],provincia_id=row['provincia_id'])
+#         canton.save()
+
+with open('datos/parroquias.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        canton = Canton.objects.create(nombre=row['canton'],provincia_id=row['provincia_id'])
-        canton.save()
+        parroquia = Parroquia.objects.create(nombre=row['parroquia'],canton_id=row['id_canton'])
+        parroquia.save()
